e09b147e43240dda5f7165072cafeeaa0bd04cd1
RohSeungHwan <45633141+foxsayy@users.noreply.github.com>
2020-01-18 10:11:51 +0900

    -

--- InsideJS/ch03_15.js
+++
@@ -1,8 +0,0 @@
-var arr = [];
-console.log(arr.length); // 0
-
-arr[0] = 0; // arr.length = 1
-arr[1] = 1; // arr.length = 2
-arr[2] = 2; // arr.length = 3
-arr[100] = 100; 
-console.log(arr.length); // 101
